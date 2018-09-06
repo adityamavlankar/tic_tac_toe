0140e87cac73e5dc6c962555e93855f258e7d7ef
Aditya Mavlankar <aditya.mavlankar@gmail.com>
2018-09-05 22:17:03 -0700

    1. more concise with list comprehensions 2. if checks don't use words True and False 3. variable names are more consistent with Python style 4. unicode chars in UI messages about game win

--- tic_tac_toe_concise.py
+++ tic_tac_toe_concise.py
@@ -9,35 +9,36 @@ from tkinter import *
 grid_size = 3
 buttons = [[None for x in range(grid_size)] for x in range(grid_size)]
 win_highlight_color = {"x" : '#3E2129', "o" : '#214129'}
+ui_symbol_unicode = {"x" : '\N{multiplication x}', "o" : '\N{large circle}'}
 button_highlight_default = '#99AA99'
 turn = True
 game_over = False
 
-def reset_game(topWindow):
+def reset_game(top_window):
     '''
-    Destroy toast and start new game
+    Destroy message window from previous game and start new game
     '''
     global game_over
     game_over = False
     for button_row in buttons:
         for button in button_row:
-            button.configure(text = " ", highlightbackground=button_highlight_default)
-    topWindow.destroy()
+            button.configure(text=" ", highlightbackground=button_highlight_default)
+    top_window.destroy()
 
 def show_message(title, label, button_text):
     '''
-    Show toast with desired title, main message and button
+    Show message window with desired title, main message and button
     '''
-    topWindow = Toplevel(tk)
-    topWindow.title(title)
-    Label(topWindow, text = label).grid()
-    Button(topWindow, text = button_text, command=lambda:reset_game(topWindow)).grid()
-    topWindow.protocol("WM_DELETE_WINDOW", lambda:reset_game(topWindow))    
+    top_window = Toplevel(tk)
+    top_window.title(title)
+    Label(top_window, text=label).grid()
+    Button(top_window, text=button_text, command=lambda:reset_game(top_window)).grid()
+    top_window.protocol("WM_DELETE_WINDOW", func=lambda:reset_game(top_window))
 
 def check_list_for_same_symbol(list_buttons, symbol):
     '''
     Check whether list of buttons passed to this function has same symbol.
-    If yes highlight those buttons and show message about the win.
+    If yes, highlight those buttons and show message about the win.
     '''
     global game_over
     for button in list_buttons:
@@ -45,7 +46,7 @@ def check_list_for_same_symbol(list_buttons, symbol):
             return False
     for button in list_buttons:
         button.configure(highlightbackground=win_highlight_color[symbol])
-    show_message('Game Over', "Winner " + symbol + ", you just won a game", 'Ok')
+    show_message('Game Over', f'Player {ui_symbol_unicode[symbol]}, you just won a game!', 'Ok')
     game_over = True
     return True
 
@@ -54,23 +55,17 @@ def check_winner(symbol):
     Check whether player with given symbol has won the game. If so, give indication in UI as such.
     '''
     for button_row in buttons:
-        if True == check_list_for_same_symbol(button_row, symbol):
+        if check_list_for_same_symbol(button_row, symbol):
             return
     for col in range(grid_size):
-        col_buttons = []
-        for row in range(grid_size):
-            col_buttons.append(buttons[row][col])
-        if True == check_list_for_same_symbol(col_buttons, symbol):
+        col_buttons = [buttons[row][col] for row in range(grid_size)]
+        if check_list_for_same_symbol(col_buttons, symbol):
             return
-    forward_diag_buttons = []
-    for i in range(grid_size):
-        forward_diag_buttons.append(buttons[i][i])
-    if True == check_list_for_same_symbol(forward_diag_buttons, symbol):
+    forward_diag_buttons = [buttons[i][i] for i in range(grid_size)]
+    if check_list_for_same_symbol(forward_diag_buttons, symbol):
         return
-    reverse_diag_buttons = []
-    for i in range(grid_size):
-        reverse_diag_buttons.append(buttons[i][grid_size - i - 1])
-    if True == check_list_for_same_symbol(reverse_diag_buttons, symbol):
+    reverse_diag_buttons = [buttons[i][grid_size - i - 1] for i in range(grid_size)]
+    if check_list_for_same_symbol(reverse_diag_buttons, symbol):
         return
         
 def check_tie():
@@ -81,26 +76,26 @@ def check_tie():
         for button in button_row:
             if button["text"] == " ":
                 return
-    show_message('Game Over', "You fought well, it is a tie!", 'Ok')
+    show_message('Game Over', 'You fought well, it is a tie!', 'Ok')
     game_over = True
 
 def checker(button):
     '''
     Process the next button click, i.e., next move in the game.
     '''
-    if game_over == True:
+    if game_over:
         return
     global turn
-    if button["text"] == " " and turn == True:
+    if button["text"] == " " and turn:
         button["text"] = "x"
         turn = False
-    elif button["text"] == " " and turn == False:
+    elif button["text"] == " " and not turn:
         button["text"] = "o"
         turn = True
 
     check_winner("x")
     check_winner("o")
-    if game_over == False:
+    if not game_over:
         check_tie()
 
 if __name__ == "__main__":
@@ -114,10 +109,10 @@ if __name__ == "__main__":
                                     highlightbackground=button_highlight_default)
             buttons[row][col].grid(row=row, column=col, sticky=S+N+E+W)
 
-    topWindowAbout = Toplevel(tk)
-    topWindowAbout.title('About')
-    Label(topWindowAbout, text="Tic Tac Toe v1.0\n\nby\n\nAditya Mavlankar\n\n\nQA tested by Aneesh and Aditya\non Mac OSX 10.11.6").grid()
-    topWindowAbout.focus_force()
+    top_window_about = Toplevel(tk)
+    top_window_about.title('About')
+    Label(top_window_about, text="Tic Tac Toe v1.0\n\nby\n\nAditya Mavlankar\n\n\nQA tested by Aneesh and Aditya\non Mac OSX 10.11.6").grid()
+    top_window_about.focus_force()
 
     tk.mainloop()
 
